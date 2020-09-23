066fd0a60543f44f2ce254ee6ad34603c2f139c1
Eric Lamb <eric@mithra62.com>
2020-09-23 09:51:08 -0700

    abstracts report compilation into object

---
+++ Report.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Git Commit Report Object
+Generates a Dictionary about a repository
+Example:
+    $ python commit-track.py
+"""
+from git import Repo
+import datetime
+import csv
+
+class Report:
+
+	def __init__(self):
+		self.repo = ''
+		self.commits = ''
+
+	def parse(self, repo_path = '',  name = '', limit = 10000000 ):
+		repo = Repo(repo_path)
+		report = {}
+		count = 0
+		for commit in repo.iter_commits( ):
+			count += 1
+			s = commit.stats
+			total = s.total
+			authored_date = datetime.datetime.fromtimestamp(
+				int(commit.authored_date)
+			).strftime('%Y-%m')
+
+			if name != None:
+				if commit.author.name != name:
+					continue
+
+			if authored_date not in report.keys():
+				report[authored_date] = {'monthyear':authored_date, 'insertions':0, 'deletions':0, 'lines':0, 'files':0, 'commits':0}
+
+			report[authored_date]['insertions'] += total['insertions']
+			report[authored_date]['deletions'] += total['deletions']
+			report[authored_date]['lines'] += total['lines']
+			report[authored_date]['files'] += total['files']
+			report[authored_date]['commits'] += 1
+			if count >= limit:
+				break
+
+		self.commits = report
+
+	def save(self, save_path ):
+		csv_columns = ['monthyear','insertions','deletions','lines','files','commits']
+		with open(save_path, 'w') as csvfile:
+			writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+			writer.writeheader()
+			for data in self.commits:
+				writer.writerow(self.commits[data])
--- commit-track.py
+++ commit-track.py
@@ -12,50 +12,24 @@ import sys
 import datetime
 import csv
 import argparse
+from Report import Report
 
-def main( repo = '', name = '' , limit = 10000000, csv_file = "commits.csv"):
-	repo = Repo(repo)
-	report = {}
-	count = 0
-
-	for commit in repo.iter_commits( ):
-		count += 1
-		s = commit.stats
-		total = s.total
-		authored_date = datetime.datetime.fromtimestamp(
-	        int(commit.authored_date)
-	    ).strftime('%Y-%m')
-
-		if name != None:
-			if commit.author.name != name:
-				continue
-
-		if authored_date not in report.keys():
-			report[authored_date] = {'monthyear':authored_date, 'insertions':0, 'deletions':0, 'lines':0, 'files':0, 'commits':0}
-
-		report[authored_date]['insertions'] += total['insertions']
-		report[authored_date]['deletions'] += total['deletions']
-		report[authored_date]['lines'] += total['lines']
-		report[authored_date]['files'] += total['files']
-		report[authored_date]['commits'] += 1
-		if count >= limit:
-			break
-
-	csv_columns = ['monthyear','insertions','deletions','lines','files','commits']
-	with open(csv_file, 'w') as csvfile:
-		writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-		writer.writeheader()
-		for data in report:
-			writer.writerow(report[data])
-
-if __name__ == "__main__":
-
+def main():
+	
 	parser = argparse.ArgumentParser(description='A tutorial of argparse!')
 	parser.add_argument("--repo", required=True, type=str, help="Full system path to the repo to report on")
 	parser.add_argument("--name", required=False, type=str, help="A specific commiter you want results from")
+	parser.add_argument("--file", required=False, default="commits.csv", type=str, help="A specific commiter you want results from")
+	parser.add_argument("--limit", required=False, default=100000000, type=int, help="How many results to return")
 	args = parser.parse_args()
 
 	if os.path.isdir(args.repo) == False:
 		raise Exception("That repository can't be found :( ")
 
-	main(repo=args.repo, name=args.name)
+	report = Report()
+	report.parse(args.repo, args.name, args.limit)
+	report.save(args.file)
+	sys.exit()
+
+if __name__ == "__main__":
+	main()
